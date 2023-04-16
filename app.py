733850b37037217138567f1d60f46bb3e40f3064
shivanandvp-official <shivanandvp.official@gmail.com>
2023-04-16 06:49:37 -0500

    .

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def receive_request():
         return response, 400
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=os.getenv('PORT'))
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=os.getenv('PORT'))
+    # app.run(host="127.0.0.1", port=5000)
